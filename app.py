9b7b352add9c53821e935646366a4b0d1d86980d
Koonavoot Kaewnopparat <169038824+Koonavoot@users.noreply.github.com>
2024-11-04 03:12:03 +0700

    add description

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ def main():
         else:
             penalty = 'l2'
         
-        max_iter = st.sidebar.number_input("Max_iter",50,500,step=10,key ='max_iter')
+        max_iter = st.sidebar.number_input("Max_iter (50-500)",50,500,step=10,key ='max_iter')
         
         metrics = st.sidebar.multiselect("What metrics to plot?", ("Confusion Matrix","ROC Curve","Precision-Recall Curve"),key='metrics')
         
